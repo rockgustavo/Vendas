62362f8ad64332d1beaef8849377faa78cf82859
rockgustavo <grcuritiba@gmail.com>
2022-07-31 20:13:11 -0300

    31-07-2022

--- src/main/java/com/test/resources/HomeController.java
+++ src/main/java/com/test/resources/HomeController.java
@@ -1,15 +1,27 @@
 package com.test.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.test.entities.User;
+
 @RestController
 public class HomeController {
+	ModelAndView mv = new ModelAndView();
 	
 	@GetMapping("/")
 	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/index");
+		return mv;
+	}
+	
+	@PostMapping("/logar")
+	public ModelAndView create(User user) {
+		
+		System.out.println("LOGIN: " + user.getName() + " - SENHA: " + user.getPassword());
+		
 		mv.setViewName("home/index");
 		return mv;
 	}
